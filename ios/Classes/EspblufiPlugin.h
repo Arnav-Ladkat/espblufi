b9137ef9b2a102b04c946527259af41e0b3ac7ac
arnav-ladkat <arnav@hlth.dev>
2022-08-21 16:52:23 +0530

    Initial Commit

---
+++ ios/Classes/EspblufiPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface EspblufiPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/EspblufiPlugin.m
@@ -0,0 +1,20 @@
+#import "EspblufiPlugin.h"
+
+@implementation EspblufiPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"espblufi"
+            binaryMessenger:[registrar messenger]];
+  EspblufiPlugin* instance = [[EspblufiPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
